5ca7ed79c652dd49191cb39fa944530e1e00d94e
Jose Miguel Garcia <josegarcia1131884@corre.itm.edu.co>
2026-09-17 12:02:10 -0500

    Eleventh point of the workshop

---
+++ Average_of_Four/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AverageOfFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresá el primer número: ");
+            double n1 = double.Parse(Console.ReadLine()!);
+            Console.Write("Ingresá el segundo número: ");
+            double n2 = double.Parse(Console.ReadLine()!);
+            Console.Write("Ingresá el tercer número: ");
+            double n3 = double.Parse(Console.ReadLine()!);
+            Console.Write("Ingresá el cuarto número: ");
+            double n4 = double.Parse(Console.ReadLine()!);
+
+            double promedio = (n1 + n2 + n3 + n4) / 4;
+            Console.WriteLine($"Resultado: {promedio}");
+        }
+    }
+}
