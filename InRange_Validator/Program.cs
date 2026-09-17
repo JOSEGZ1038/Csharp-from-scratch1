3c8eaf15bfafd536f300fd56ca4b6b7e47215997
Jose Miguel Garcia <josegarcia1131884@corre.itm.edu.co>
2026-09-17 12:25:28 -0500

    Fifteenth point of the workshop

---
+++ InRange_Validator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InRangeValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresá un número: ");
+            int n = int.Parse(Console.ReadLine()!);
+
+            if (n >= 10 && n <= 20)
+            {
+                Console.WriteLine("Está en el rango.");
+            }
+            else
+            {
+                Console.WriteLine("Fuera del rango.");
+            }
+        }
+    }
+}
