16e4c61dbe285e4bd37249d68bbfad3ef5025cca
Jose Miguel Garcia <josegarcia1131884@corre.itm.edu.co>
2026-09-17 11:40:01 -0500

    Eighth point of the workshop

---
+++ Sum_of_Evens/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumOfEvens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    suma += i;
+                }
+            }
+
+            Console.WriteLine($"Resultado: {suma}");
+        }
+    }
+}
