be563380d2272c1d3da0ad5901c5efb30a93cca8
Jose Miguel Garcia <josegarcia1131884@corre.itm.edu.co>
2026-09-17 11:55:08 -0500

    Tenth point of the workshop

---
+++ String_Length/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresá una palabra: ");
+            string palabra = Console.ReadLine()!;
+
+            Console.WriteLine("Resultado: " + palabra.Length);
+        }
+    }
+}
